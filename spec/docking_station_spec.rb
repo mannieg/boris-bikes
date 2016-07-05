d4e20b16de3ac6c77cff5cb1565b192a5efb7bb0
Luke Cartledge <luke@lukecartledge.com>
2016-07-05 17:28:15 +0100

    passed inital test - created class and first def

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,8 @@
-describe DockingStation do
+require 'docking_station'
 
+describe DockingStation do
+  it 'responds to request to release a bike' do
+    station = DockingStation.new
+    expect(station).to respond_to(release_bike)
+  end
 end
