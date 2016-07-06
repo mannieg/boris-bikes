fb96b38b788a80b765a0a99015cbce2b8a364c12
Jose <jcopoviking@outlook.com>
2016-07-06 15:09:12 +0100

    passes is bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-end
-
-describe 'release_bike' do
-  it 'releases a working bike' do
+  it 'checks bike is working?' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
