d04b3375cbfe8dfed41f669eb0d730e8cc6c7af6
Jose <jcopoviking@outlook.com>
2016-07-06 12:40:17 +0100

    changes docking_station_spec to one line Rspec syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-   it 'responds to request to release a bike' do
-     expect(subject).to respond_to(:release_bike)
-   end
+  it {is_expected.to respond_to :release_bike}
 end
