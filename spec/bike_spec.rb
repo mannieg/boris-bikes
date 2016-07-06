4ac28cf9b1ab2e5079a6257eaadf1455a245ab20
Luke Cartledge <luke@lukecartledge.com>
2016-07-06 14:18:51 +0100

    starts new test for working bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,5 @@
 require "bike"
 
 describe Bike do
-  it 'responds to request to see whether a bike is working' do
-       expect(subject).to respond_to(:working?)
-
-  end
+  it {is_expected.to respond_to :working?}
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,8 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 end
+
+describe 'release_bike' do
+  it 'releases a working bike' do
+  end
+end
