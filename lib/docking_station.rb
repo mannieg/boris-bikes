480ce5a1ee2bbb50bcb20aeacebaa4d3582af916
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-07 17:01:21 +0100

    Implemented exception handling if no bikes are available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,15 +5,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    raise 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
     @bike = bike
   end
-
-  # def return_bike
-  #   @bike
-  # end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,18 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)}
 
-  it 'release a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   it { is_expected.to respond_to(:dock).with(1).arguments}
@@ -17,6 +26,6 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to be_kind_of(Bike)
   end
-  #it {expect(subject.return_bike).to be_kind_of(bike)}
 
+#  it { expect(subject.release_bike).to raise_error("Sorry no bike is available!") }
 end
